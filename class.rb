b8fff79fcc644504d1593e8332bb970934eac02b
SrinivasaSaran <121004244@sastra.ac.in>
2020-04-17 16:34:12 +0530

    Update: Tell Dont Ask

--- class.rb
+++ class.rb
@@ -1,3 +1,4 @@
+#Example 1
 class Student
   def initialize(roll_no, name)
     @roll_no = roll_no
@@ -7,7 +8,7 @@ class Student
 
   #setter:
   def enroll
-    @is_enrolled = "tru"
+    @is_enrolled = true
   end
 
   #These methods, like enroll, which "sets" some value inside an
@@ -31,7 +32,44 @@ student = Student.new(715, "Raj") #Creating object for Student class
 #puts student.name
 #Above line gives error as instance varible is private to
 #the object and only instance methods can alter,access them.
-puts student.enroll
+student.enroll
 student2 = Student.new(716, "Raju")
 student2.enroll
 puts student2.name
+
+#Example 2
+
+#Violation of 'tell dont ask'
+class Course
+  def initialize(name, students)
+    @name = name
+    @students = students
+  end
+
+  def students
+    @students
+  end
+
+  def students=(s)
+    @students = s
+  end
+end
+
+c = Course.new("SaaS", ["A", "B"])
+c.students = c.students.concat(["X", "Y"]) # this calls `students=` method
+
+#obeying 'tell dont ask'
+class Course
+  def initialize(name, students)
+    @name = name
+    @students = students
+  end
+
+  def add_student(student)
+    @students.push(student)
+  end
+end
+
+c = Course.new("SaaS", ["A", "B"])
+c.add_student("X")
+c.add_student("Y")
