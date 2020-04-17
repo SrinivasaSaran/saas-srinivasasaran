58c1f2692579f4a28fd79f17345a8035b8b28f17
SrinivasaSaran <121004244@sastra.ac.in>
2020-04-17 22:52:40 +0530

    Another way of declaring Hash

---
+++ L3_todo_list/learner.rb
@@ -0,0 +1,8 @@
+require "date"
+date = Date.today
+todos = [
+  { text: "Submit assignment", due_date: date - 1, completed: false },
+  { text: "Pay rent", due_date: date, completed: true },
+  { text: "File taxes", due_date: date + 1, completed: false },
+  { text: "Call Acme Corp.", due_date: date + 1, completed: false },
+]
