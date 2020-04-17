c1b66abe09e921b9863548e8576b175e017d680b
SrinivasaSaran <121004244@sastra.ac.in>
2020-04-17 13:33:43 +0530

    Update: Getters in class

--- class.rb
+++ class.rb
@@ -5,16 +5,33 @@ class Student
     @is_enrolled = false
   end
 
+  #setter:
   def enroll
-    @is_enrolled = true
+    @is_enrolled = "tru"
   end
 
   #These methods, like enroll, which "sets" some value inside an
   #object are called "setters" in object-oriented programming.
+
+  #getters:
+  def is_enrolled?
+    @is_enrolled
+  end
+
+  def name
+    @name
+  end
+
+  def roll_no
+    @roll_no
+  end
 end
 
 student = Student.new(715, "Raj") #Creating object for Student class
 #puts student.name
 #Above line gives error as instance varible is private to
 #the object and only instance methods can alter,access them.
-student.enroll # Aliter: 'student = Student.new(715, "Raj").enroll'
+puts student.enroll
+student2 = Student.new(716, "Raju")
+student2.enroll
+puts student2.name
