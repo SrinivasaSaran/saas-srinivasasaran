18295411379148ded0811cc124964f3bd1469f7c
SrinivasaSaran <121004244@sastra.ac.in>
2020-04-17 13:11:41 +0530

    Update: Setters in class

--- class.rb
+++ class.rb
@@ -4,8 +4,17 @@ class Student
     @name = name
     @is_enrolled = false
   end
+
+  def enroll
+    @is_enrolled = true
+  end
+
+  #These methods, like enroll, which "sets" some value inside an
+  #object are called "setters" in object-oriented programming.
 end
 
 student = Student.new(715, "Raj") #Creating object for Student class
-#puts student.name # This gives error as instance varible is private
-#to the object and only instance methods can alter,access them.
+#puts student.name
+#Above line gives error as instance varible is private to
+#the object and only instance methods can alter,access them.
+student.enroll # Aliter: 'student = Student.new(715, "Raj").enroll'
