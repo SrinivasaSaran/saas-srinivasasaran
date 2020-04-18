8cbe6a90b0207c3d3ce926246ed2a12f4e98ceab
SrinivasaSaran <121004244@sastra.ac.in>
2020-04-19 01:36:20 +0530

    list-todos.rb will work now

---
+++ L4-ActiveRecord/L4_Milestone/todo.rb
@@ -0,0 +1,37 @@
+require "active_record"
+
+class Todo < ActiveRecord::Base
+  def overdue
+    due_date < Date.today
+  end
+
+  def due_today
+    due_date == Date.today
+  end
+
+  def due_later
+    due_date > Date.today
+  end
+
+  def to_displayable_string
+    display_status = completed ? "[X]" : "[ ]"
+    display_date = due_today ? nil : due_date
+    "#{id}. #{display_status} #{todo_text} #{display_date}"
+  end
+
+  def self.show_list
+    puts "\n\nMy Todo-list\n\n"
+
+    puts "Overdue\n"
+    puts all.select { |todo| todo.overdue }.map { |todo| todo.to_displayable_string }
+    puts "\n\n"
+
+    puts "Due Today\n"
+    puts all.select { |todo| todo.due_today }.map { |todo| todo.to_displayable_string }
+    puts "\n\n"
+
+    puts "Due Later\n"
+    puts all.select { |todo| todo.due_later }.map { |todo| todo.to_displayable_string }
+    puts "\n\n"
+  end
+end
